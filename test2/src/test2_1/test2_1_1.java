7bab9edc09a7cc3587d6a9210eff1acedcfa0c43
guochao <1029891430@qq.com>
2021-04-06 15:19:55 +0800

    111

---
+++ test2/src/test2_1/test2_1_1.java
@@ -0,0 +1,11 @@
+package test2_1;
+
+public class test2_1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+		System.out.println("stady up!");
+	}
+
+}
